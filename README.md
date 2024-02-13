a21c271fcd2ca3cbcf5bd5608a56d0e61d998738
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-02-13 13:02:47 -0600

    added iam policy to terraform and updated readme
    Co-authored-by: Jeff Grenard <jeff@orendaed.org>

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # websocketTestingPractice
 practicing with websockets and (hopefully) testing
 
-Potentially Helpful Resources
+# Potentially Helpful Resources
 https://quii.gitbook.io/learn-go-with-tests/build-an-application/websockets
 https://spacelift.io/blog/terraform-gitignore
+https://awstip.com/websocket-api-gateway-with-terraform-8a509585121d
--- main.tf
+++ main.tf
@@ -1,24 +1,22 @@
 resource "aws_apigatewayv2_api" "example" {
   name                       = "example-websocket-api"
   protocol_type              = "WEBSOCKET"
-  
-
 }
 
-
 output "test" {
-    value = aws_apigatewayv2_api.example.api_endpoint
-  }
+  value = aws_apigatewayv2_api.example.api_endpoint
+}
 
 resource "aws_apigatewayv2_route" "ws_messenger_api_connect_route" {
   api_id    = aws_apigatewayv2_api.example.id
-  route_key = "$default"
-  
+  route_key = "$connect"
+  target = "integrations/${aws_apigatewayv2_integration.example.id}"
+  route_response_selection_expression = "$default"
 }
 
 resource "aws_apigatewayv2_integration" "example" {
   api_id           = aws_apigatewayv2_api.example.id
-  integration_type = "MOCK"
+  integration_type = "MOCK"// we don't MOCK - AWS_PROXY insteadt  
 }
 
 resource "aws_apigatewayv2_stage" "example" {
@@ -31,3 +29,26 @@ resource "aws_apigatewayv2_route_response" "example" {
   route_id           = aws_apigatewayv2_route.ws_messenger_api_connect_route.id
   route_response_key = "$default"
 }
+
+resource "aws_apigatewayv2_integration_response" "connectIntegrationReponse" {
+  api_id                   = aws_apigatewayv2_api.example.id
+  integration_id           = aws_apigatewayv2_integration.example.id
+  integration_response_key = "$default"
+  template_selection_expression = "xxx"
+  response_templates = {
+    "$default" = "{ statusCode: 200 }"
+    }
+}
+
+data "aws_iam_policy_document" "ws_lambda_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect = "Allow"
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
