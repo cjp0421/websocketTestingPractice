84bb6b24653053bdd70248d32867f7e59c9abb1c
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-02-13 15:37:24 -0600

    Co-authored-by: Jeff Grenard <jeff@orendaed.org>

--- main.tf
+++ main.tf
@@ -34,21 +34,8 @@ resource "aws_apigatewayv2_integration_response" "connectIntegrationReponse" {
   api_id                   = aws_apigatewayv2_api.example.id
   integration_id           = aws_apigatewayv2_integration.example.id
   integration_response_key = "$default"
-  template_selection_expression = "xxx"
+  # template_selection_expression = "xxx" // Might not need this if all we want back is the status code?
   response_templates = {
     "$default" = "{ statusCode: 200 }"
     }
 }
-
-data "aws_iam_policy_document" "ws_lambda_policy" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    effect = "Allow"
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
-
